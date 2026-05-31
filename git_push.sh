1c40fbd2a2603c166492d1b346e9a842e8e54f39
pavlenkorv <pavlenko.rv@gmail.com>
2026-05-31 16:28:23 +0200

    Update dashboard data

--- git_push.sh
+++ git_push.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env zsh
 set -e
-
 MSG="${1:-Update dashboard data}"
-
 git add .
-
 if git diff --cached --quiet; then
-  echo "Nothing to commit, working tree clean"
+  echo "Nothing to commit"
 else
   git commit -m "$MSG"
 fi
-
 git pull --rebase origin main
-
 git push origin main
